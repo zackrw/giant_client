98e9ed26d756d99a9620abfeb70ce94195803af2
Zack Reneau-Wedeen <xaq2892@yahoo.com>
2012-08-10 18:38:23 -0400

    manual require in spec_helper for clarity

--- lib/giantclient.rb
+++
@@ -1,1 +0,0 @@
-require File.expand_path('../giant_client', __FILE__)
--- spec/examples/spec_helper.rb
+++ spec/examples/spec_helper.rb
@@ -1,4 +1,6 @@
 require 'bundler'
 require 'webmock/rspec'
 
+require File.expand_path( '../../../lib/giant_client', __FILE__ )
+
 Bundler.require( :default, :development )
